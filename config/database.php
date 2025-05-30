02f130087d4c6affaaea9561470d6ad57e7287bc
V4LC1R <valcir212@gmail.com>
2025-05-30 10:15:52 -0400

    finalizando configuracao da aplicacao

--- config/database.php
+++ config/database.php
@@ -31,18 +31,22 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
+        'pgsql' => [
+            'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-            'busy_timeout' => null,
-            'journal_mode' => null,
-            'synchronous' => null,
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
         ],
 
-        'mysql' => [
+        'graduacao' => [
             'driver' => 'mysql',
             'url' => env('DW_URL'),
             'host' => env('DW_HOST', '127.0.0.1'),
@@ -60,6 +64,37 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+        ],
+
+        'pos-graduacao' => [
+            'driver' => 'mysql',
+            'url' => env('DW_URL'),
+            'host' => env('DW_HOST', '127.0.0.1'),
+            'port' => env('DW_PORT', '3306'),
+            'database' => env('DW_DATABASE', 'laravel'),
+            'username' => env('DW_USERNAME', 'root'),
+            'password' => env('DW_PASSWORD', ''),
+            'unix_socket' => env('DW_SOCKET', ''),
+            'charset' => env('DW_CHARSET', 'utf8mb4'),
+            'collation' => env('DW_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DB_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+            'busy_timeout' => null,
+            'journal_mode' => null,
+            'synchronous' => null,
         ],
 
         'mariadb' => [
@@ -82,20 +117,7 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
-        ],
+        
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
