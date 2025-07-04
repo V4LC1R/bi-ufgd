48c39621e64708d3b4ec2978abd93805d364d5bc
V4LC1R <valcir212@gmail.com>
2025-07-03 20:39:58 -0400

    Finalizando Aquitetura

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->routes(function () {
+            $this->mapModuleRoutes();
+        });
+    }
+
+    protected function mapModuleRoutes(): void
+    {
+        $modulesPath = base_path('app/Modules');
+        $modules = File::directories($modulesPath);
+
+        foreach ($modules as $modulePath) {
+            $moduleName = basename($modulePath);
+            $routesPath = $modulePath . '/Routes';
+
+            if (File::exists($routesPath . '/api.php')) {
+                Route::prefix(strtolower($moduleName))
+                    ->middleware('api')
+                    ->group($routesPath . '/api.php');
+            }
+
+            if (File::exists($routesPath . '/web.php')) {
+                Route::middleware('web')
+                    ->group($routesPath . '/web.php');
+            }
+        }
+    }
+}
